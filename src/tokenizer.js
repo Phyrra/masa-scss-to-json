a83f563f98bec1c55f5a0da21252ca20ca6be4c6
Cabadath <>
2018-07-27 22:16:24 +0200

    Change handling of multiple paths

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -45,21 +45,20 @@ class Tokenizer {
 					return false;
 				}
 
-				if (results.length > 1) {
-					console.log(
-						'FOUND_MATCHES',
-						results.map(result =>
-							result.tokens.map(
-								token => token.token + ' [' + token.match.slice(1).join(', ') + ']'
-							)
-						)
+				/*
+				 * Edge case:
+				 * Statement ends in [ { empty: true }, { token: Token } ]
+				 * This will produce 2 possible paths,
+				 * in this case take the longest one
+				 */
+				const longestMatch = results
+					.reduce(
+						(longest/*: IResult*/, match/*: IResult*/) => match.tokens.length > longest.tokens.length ? match : longest,
+						{ tokens: [] }
 					);
 
-					throw new Error(`Multiple matches for ${oneLine}`);
-				}
-
-				oneLine = results[0].line;
-				tokens = tokens.concat(results[0].tokens);
+				oneLine = longestMatch.line;
+				tokens = tokens.concat(longestMatch.tokens);
 
 				return true;
 			});
@@ -92,7 +91,7 @@ class Tokenizer {
 		const part/*: StatementNode | StatementNode[]*/ = statement[i];
 
 		if (Array.isArray(part)) {
-			const partials = part
+			part
 				.map((option/*: StatementNode*/) => {
 					if (option.canRepeat) {
 						throw new Error(`Repeat in options is not allowed`);
@@ -100,17 +99,7 @@ class Tokenizer {
 
 					return this._canMatchPart(option, line);
 				})
-				.reduce((allPartialMatches, partials) => allPartialMatches.concat(partials), []);
-
-			// In case of multiple matches, throw away empty ones
-			let filteredPartials/*: IResult[] */;
-			if (partials.length > 1) {
-				filteredPartials = partials.filter((partial/*: IResult*/) => partial.tokens.length > 0);
-			} else {
-				filteredPartials = partials;
-			}
-
-			filteredPartials
+				.reduce((allPartialMatches, partials) => allPartialMatches.concat(partials), [])
 				.forEach((partial/*: IResult*/) => results = results.concat(
 					this._canMatchStatement(statement, i + 1, partial.line, tokens.concat(partial.tokens))
 				));
