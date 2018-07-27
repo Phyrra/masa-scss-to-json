fee50c2d67094d03739fbfdbd62ba2cac5521b81
Cabadath <masa@nbagmbp042.local>
2018-07-27 12:59:59 +0200

    Concat result arrays instead of passing result-placeholder around

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -34,18 +34,17 @@ class Tokenizer {
 	travelAst(root/*: Statement[]*/, lines/*: string[]*/)/*: IToken[] */ {
 		// Screw whitespace, let's do it later
 		let oneLine/*: string*/ = lines.join('\n').trim();
-	
+
 		let tokens/*: IToken[]*/ = [];
-	
+
 		while (oneLine.length > 0) {
 			const found/*: boolean*/ = root.some((statement/*: Statement*/) => {
-				const results/*: IResult[]*/ = [];
-				this._canMatchStatement(statement, 0, oneLine, [], results);
-	
+				const results/*: IResult[]*/ = this._canMatchStatement(statement, 0, oneLine, []);
+
 				if (results.length === 0) {
 					return false;
 				}
-	
+
 				if (results.length > 1) {
 					console.log(
 						'FOUND_MATCHES',
@@ -55,54 +54,54 @@ class Tokenizer {
 							)
 						)
 					);
-	
+
 					throw new Error(`Multiple matches for ${oneLine}`);
 				}
-	
+
 				oneLine = results[0].line;
 				tokens = tokens.concat(results[0].tokens);
-	
+
 				return true;
 			});
-	
+
 			if (!found) {
 				console.log(
 					'FOUND_TOKENS',
 					tokens.map(token => token.token + ' [' + token.match.slice(1).join(', ') + ']')
 				);
-	
+
 				throw new Error(`Could not match ${oneLine}`);
 			}
 		}
-	
+
 		//console.log(tokens.map(token => token.token + ' [' + token.match.slice(1).join(', ' + ']')));
-	
+
 		return tokens;
 	}
-	
-	_canMatchStatement(statement/*: Statement*/, i/*: number*/, line/*: string*/, tokens/*: IToken[]*/, results/*: IResult[]*/)/*: void*/ {
+
+	_canMatchStatement(statement/*: Statement*/, i/*: number*/, line/*: string*/, tokens/*: IToken[]*/)/*: IResult[]*/ {
 		if (i >= statement.length) {
-			results.push({
+			return [{
 				line: line,
 				tokens: tokens
-			});
-	
-			return;
+			}];
 		}
-	
+
+		let results/*: IResult[]*/ = [];
+
 		const part/*: StatementNode | StatementNode[]*/ = statement[i];
-	
+
 		if (Array.isArray(part)) {
 			const partials = part
 				.map((option/*: StatementNode*/) => {
 					if (option.canRepeat) {
 						throw new Error(`Repeat in options is not allowed`);
 					}
-	
+
 					return this._canMatchPart(option, line);
 				})
 				.reduce((allPartialMatches, partials) => allPartialMatches.concat(partials), []);
-	
+
 			// In case of multiple matches, throw away empty ones
 			let filteredPartials/*: IResult[] */;
 			if (partials.length > 1) {
@@ -110,35 +109,36 @@ class Tokenizer {
 			} else {
 				filteredPartials = partials;
 			}
-	
+
 			filteredPartials
-				.forEach(
-					(partial/*: IResult*/) => this._canMatchStatement(statement, i + 1, partial.line, tokens.concat(partial.tokens), results)
-				);
-	
-			return;
+				.forEach((partial/*: IResult*/) => results = results.concat(
+					this._canMatchStatement(statement, i + 1, partial.line, tokens.concat(partial.tokens))
+				));
+		} else {
+			this._canMatchPart(part, line)
+				.forEach((partial/*: IResult*/) => {
+					if (part.canRepeat) {
+						results = results.concat(
+							this._canMatchStatement(statement, i, partial.line, tokens.concat(partial.tokens))
+						);
+					}
+
+					results = results.concat(
+						this._canMatchStatement(statement, i + 1, partial.line, tokens.concat(partial.tokens))
+					);
+				});
 		}
-	
-		this._canMatchPart(part, line)
-			.forEach((partial/*: IResult*/) => {
-				if (part.canRepeat) {
-					this._canMatchStatement(statement, i, partial.line, tokens.concat(partial.tokens), results);
-				}
-	
-				this._canMatchStatement(statement, i + 1, partial.line, tokens.concat(partial.tokens), results)
-			});
+
+		return results;
 	}
-	
+
 	_canMatchPart(part/*: RuleNode */, line/*: string*/)/*: IResult[] */ {
 		//console.log('matching', part, 'against', line);
-	
+
 		if (part.statement) {
-			const nestedResults/*: IResult[]*/ = [];
-			this._canMatchStatement(part.statement, 0, line, [], nestedResults);
-	
-			return nestedResults;
+			return this._canMatchStatement(part.statement, 0, line, []);
 		}
-	
+
 		if (part.token) {
 			const match/* string[]*/ = line.match(this._tokenDefinitions[part.token]);
 			if (match) {
@@ -150,19 +150,19 @@ class Tokenizer {
 					line: line.replace(this._tokenDefinitions[part.token], '').trim()
 				}];
 			}
-	
+
 			return [];
 		}
-	
+
 		if (part.empty) {
 			return [{
 				tokens: [],
 				line: line
 			}];
 		}
-	
+
 		throw new Error(`Unknown part`);
-	}	
+	}
 }
 
 module.exports = (tokenDefinitions/*: TokenDefinitions*/, root/*: Statement[]*/, lines/*: string[]*/) =>
